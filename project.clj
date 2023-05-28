455a2567f4a2fbf75d31b18990b065182b351a8c
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-05-28 15:12:40 +0100

    Update dependencies and config

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "https://github.com/vimanyu-taneja/irl-theory-test-qs"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[clj-http "3.12.3"]
+                 [enlive "1.1.6"]
+                 [org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot irl-theory-test-qs.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   :repl-options {:init-ns irl-theory-test-qs.core})
