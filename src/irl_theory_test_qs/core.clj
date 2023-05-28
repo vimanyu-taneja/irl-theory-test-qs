64831b033297aed4a9ca4b160898d3bd9be4db45
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-05-28 15:12:57 +0100

    Scrape a single question

--- src/irl_theory_test_qs/core.clj
+++ src/irl_theory_test_qs/core.clj
@@ -1,6 +1,51 @@
-(ns irl-theory-test-qs.core)
+(ns irl-theory-test-qs.core
+  (:require [clj-http.client :as client]
+            [clojure.string :as str]
+            [net.cgrand.enlive-html :as html]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def base-url "http://theory-tester.com/questions/")
+
+(defn extract-text
+  "Extract the text from a HTML element and sanitise it"
+  [node]
+  (-> (html/text node)
+      (str/replace #"\n|\t" "")
+      (str/replace #"\?s" "'s")))
+
+(def parsers
+  {:category    {:selector  [:.p-questionSingle-heading]
+                 :processor #(-> (first %)
+                                 (extract-text))}
+   :question    {:selector  [:.pageHeading]
+                 :processor #(-> (first %)
+                                 (extract-text))}
+   :image       {:selector  [:.quiz-question-img :> :img]
+                 :processor #(-> (first %)
+                                 (get-in [:attrs :src]))}
+   :options     {:selector  [:.options-single]
+                 :processor #(-> (map extract-text %))}
+   :answer      {:selector  [:.js-correct-answer]
+                 :processor #(-> (first %)
+                                 (extract-text))}
+   :explanation {:selector  [:.p-questionSingle-explanation]
+                 :processor #(-> (first %)
+                                 (extract-text)
+                                 (str/replace #"Explantion:\s*" ""))}})  ;; sic
+
+(defn scrape-question
+  "Construct a map of all the data for a single question"
+  [question-num]
+  (let [url         (str base-url question-num)
+        parsed-html (-> (client/get url)
+                        (:body)
+                        (html/html-snippet))]
+    (reduce (fn [result [field {:keys [selector
+                                       processor]}]]
+              (assoc result
+                     field (-> (html/select parsed-html selector)
+                               (processor))))
+            {:id question-num}
+            parsers)))
+
+(defn -main [& args]
+  (println "Running with args:" args))
